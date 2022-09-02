2f49f2c92d25bf7dbaddbe76845622a547c984ff
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-09-02 21:29:42 +0000

    update url link to product detail in product.html

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -15,7 +15,7 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-def product_details(request, product_id):
+def product_detail(request, product_id):
     """
     A view with product details
     """
@@ -25,4 +25,4 @@ def product_details(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/product_details.html', context)
+    return render(request, 'products/product_detail.html', context)
