4047aeba4766241159ca65488481d00d3654cd2e
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-09-06 20:16:39 +0000

    add add to bag view for the bag so that user can see their added products in the bag

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404
 )
+from django.contrib import messages
+from products.models import Product
+
 
 # Create your views here.
 def view_bag(request):
@@ -8,3 +11,22 @@ def view_bag(request):
     A view to return the bag page
     '''
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """
+    A view to add the item quantity to the bag
+    """
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
