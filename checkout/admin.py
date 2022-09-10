ebc177e9d7a2c3c97b2da418a4b9bc5ae492bf69
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-09-10 21:02:13 +0000

    add signals.py to update the total cost for the order

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Creates model fields for admin
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Creates the admin fields within the database.
+    """
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = ('order_number',
+                       'delivery_cost',
+                       'order_total',
+                       'grand_total',
+                       'date',
+                       'original_bag',
+                       'stripe_pid',
+                       )
+
+    fields = ('full_name',
+              'order_number',
+              'user_profile',
+              'date',
+              'phone_number',
+              'country',
+              'email',
+              'postcode',
+              'town_or_city',
+              'street_address1',
+              'street_address2',
+              'county',
+              'delivery_cost',
+              'order_total',
+              'grand_total',
+              'original_bag',
+              'stripe_pid',)
+
+    list_display = ('full_name',
+                    'order_number',
+                    'date',
+                    'delivery_cost',
+                    'order_total',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/signals.py
@@ -0,0 +1,24 @@
+'''
+Imports relevant django packages
+'''
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on line item delete
+    """
+    print('Delete signal recieved')
+    instance.order.update_total()
