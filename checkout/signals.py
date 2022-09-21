c5f9021f0fa401fe46bf9114401bc84852de0b08
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-09-21 20:51:49 +0000

    heroku deployment

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,7 +3,6 @@ Imports relevant django packages
 """
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
 from .models import OrderLineItem
 
 
@@ -18,7 +17,6 @@ def update_on_save(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
-    Update order total on line item delete
+    Update order total on lineitem delete
     """
-    print('Delete signal recieved')
     instance.order.update_total()
