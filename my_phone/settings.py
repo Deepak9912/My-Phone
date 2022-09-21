4be3f772a9cd158c0bcc4d550a79abd76f7cb7bd
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-09-21 20:58:13 +0000

    remove secret key and set debug

--- my_phone/settings.py
+++ my_phone/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8%+vh)fja2%ykz#s^(v)%0rtbfvd)w#m1%vx9@boom_yil7nl5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['my-phone2234.herokuapp.com', 'localhost']
 
