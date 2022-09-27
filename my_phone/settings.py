9a205cfe42248db7def11267a6f866690366f303
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-09-27 19:38:30 +0000

    deployment

--- my_phone/settings.py
+++ my_phone/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '0l-&kj+#1tvvu*hhlz^g8w+k!hk(f_=4xd^6zxy(kdh_req_5d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,13 +124,17 @@ WSGI_APPLICATION = 'my_phone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
