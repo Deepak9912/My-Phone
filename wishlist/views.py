13329b617ed1bfd1ee9ede0ac8ee16cc0ce5702d
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-10-09 13:18:48 +0000

    update wishlist.htl page

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from products.models import Product
-# Create your views here.
 
 
 def view_wishlist(request):
