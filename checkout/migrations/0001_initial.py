8133e12ae437cb78ef7471fe577b3d24b0f2299f
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-10-16 20:06:41 +0000

    fixed pep8 errors from checkout app

--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -16,21 +16,29 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Order',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('order_number', models.CharField(editable=False, max_length=32)),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID')),
+                ('order_number', models.CharField(
+                    editable=False, max_length=32)),
                 ('full_name', models.CharField(max_length=50)),
                 ('phone_number', models.CharField(max_length=20)),
                 ('email', models.EmailField(max_length=250)),
                 ('country', models.CharField(max_length=40)),
                 ('town_or_city', models.CharField(max_length=50)),
-                ('postcode', models.CharField(blank=True, max_length=20, null=True)),
+                ('postcode', models.CharField(
+                    blank=True, max_length=20, null=True)),
                 ('street_address1', models.CharField(max_length=100)),
-                ('street_address2', models.CharField(blank=True, max_length=100, null=True)),
+                ('street_address2', models.CharField(
+                    blank=True, max_length=100, null=True)),
                 ('county', models.CharField(max_length=100)),
                 ('date', models.DateTimeField(auto_now_add=True)),
-                ('delivery_cost', models.DecimalField(decimal_places=2, default=0, max_digits=6)),
-                ('order_total', models.DecimalField(decimal_places=2, default=0, max_digits=12)),
-                ('grand_total', models.DecimalField(decimal_places=2, default=0, max_digits=6)),
+                ('delivery_cost', models.DecimalField(
+                    decimal_places=2, default=0, max_digits=6)),
+                ('order_total', models.DecimalField(
+                    decimal_places=2, default=0, max_digits=12)),
+                ('grand_total', models.DecimalField(
+                    decimal_places=2, default=0, max_digits=6)),
                 ('original_bag', models.TextField(default='')),
                 ('stripe_pid', models.CharField(default='', max_length=254)),
             ],
@@ -38,11 +46,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='OrderLineItem',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID')),
                 ('quantity', models.IntegerField(default=0)),
-                ('lineitem_total', models.DecimalField(decimal_places=2, editable=False, max_digits=6)),
-                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.order')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.product')),
+                ('lineitem_total', models.DecimalField(
+                    decimal_places=2, editable=False, max_digits=6)),
+                ('order', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='lineitems', to='checkout.order')),
+                ('product', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    to='products.product')),
             ],
         ),
     ]
